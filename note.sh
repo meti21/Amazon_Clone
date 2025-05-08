6717400d0d701a00782a2f953eb552fd7081e8de
meti21 <metiwol@gmail.com>
2025-05-08 17:01:36 -0600

    chore: add all uncommitted changes

---
+++ note.sh
@@ -0,0 +1,17 @@
+# initial vite  set up 
+npm create vite@latest
+npm install
+npm run dev
+
+# Header
+npm install react-icons --save
+
+# Carousel
+npm i react-responsive-carousel
+
+# product
+npm i axios
+
+npm install @mui/material @mui/styled-engine-sc styled-components
+
+npm install numeral
