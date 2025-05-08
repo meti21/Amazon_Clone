b27f901279a5d23988f853b2b9a6e6170db3d990
meti21 <metiwol@gmail.com>
2025-05-08 16:29:26 -0600

    feat: add Carousel component

---
+++ src/Components/CarouselEffect/CarouselEffect.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import { CarouselImages } from "./resource/data";
+
+import styles from "./CarouselEffect.module.css";
+
+//css for react-responsive-carousel from npm documentation "usage" section
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+
+
+function CarouselEffect() {
+  return (
+    <div>
+      <Carousel
+        autoPlay={true} // Automatically starts playing the carousel on load
+        infiniteLoop={true} // Allows the carousel to loop back to the first slide after the last one
+        showIndicators={false} // Hides the small navigation dots usually shown at the bottom
+        showThumbs={false} // Hides the thumbnail previews below the carousel
+      >
+        {CarouselImages?.map((currentimageLink, i) => {
+          return <img key={i} src={currentimageLink} alt="img" />;
+        })}
+      </Carousel>
+
+      {/*fade at the bottom of the banner images  */}
+      <div className={styles.hero__img}></div>
+    </div>
+  );
+}
+
+export default CarouselEffect
