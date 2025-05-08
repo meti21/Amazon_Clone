a5076dd0181c8a3a6693ae16fa8a66ec63dc6f7d
meti21 <metiwol@gmail.com>
2025-05-08 16:33:02 -0600

    chore: add resource data

---
+++ src/Components/CarouselEffect/resource/data.js
@@ -0,0 +1,10 @@
+import tv from './images/tv.jpg'
+import alexa from "./images/alexa.jpeg";
+import audible from './images/audible.jpeg'
+import brands from './images/brands.jpeg'
+import gifts from './images/gifts.jpeg'
+import homestyle from './images/homestyle.jpg'
+import prime from './images/prime.jpg'
+
+
+export const CarouselImages= [tv,alexa,audible,brands,gifts,homestyle,prime]
