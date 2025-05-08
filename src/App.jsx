92b21641875299b7d2954cc65ad15076b860b63d
meti21 <metiwol@gmail.com>
2025-05-08 16:57:13 -0600

    feat: update App.jsx with new feature

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import Header from './Components/Header/Header'
+import CarouselEffect from './Components/CarouselEffect/CarouselEffect'
+import Category from './Components/Category/Category'
+import Product from './Components/Product/Product'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <CarouselEffect/>
+      <Category/>
+      <Product/>
+    </>
+  )
+}
+
+export default App
