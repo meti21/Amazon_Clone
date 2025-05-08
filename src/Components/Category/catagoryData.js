11511d1db56534fe4892eafc40236d1a002310af
meti21 <metiwol@gmail.com>
2025-05-08 16:40:37 -0600

    feat: add Category component

---
+++ src/Components/Category/catagoryData.js
@@ -0,0 +1,23 @@
+export const CategoryInfo = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imgLink: "https://m.media-amazon.com/images/I/71ka3DKN63L._AC_UY218_.jpg", // Modern electronics
+  },
+  {
+    title: "Discover Fashion Trends",
+    name: "women's clothing",
+    imgLink:
+      "https://m.media-amazon.com/images/G/15/IHGCA/49/ngw/nav/6._CB548652006_UC216,216_.jpg", // Fashionable outfit
+  },
+  {
+    title: "Men's Clothing",
+    name: "men's clothing",
+    imgLink: "https://m.media-amazon.com/images/I/81+oQBvBR-L._AC_SX569_.jpg", // Men's jacket
+  },
+  {
+    title: "Jewelry",
+    name: "jewelery",
+    imgLink: "https://m.media-amazon.com/images/I/61CQZPykYoL._AC_SX679_.jpg", // Elegant jewelry
+  },
+];
