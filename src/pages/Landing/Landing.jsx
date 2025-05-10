3cbbc3ae44a0ff925805b1ec35cdcce942e3bb70
meti21 <metiwol@gmail.com>
2025-05-09 22:47:39 -0600

    feat: add Landing page component

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Category from '../../Components/Category/Category'
+import Product from '../../Components/Product/Product'
+import LayOut from '../../Components/LayOut/LayOut'
+import CarouselEffect from '../../Components/CarouselEffect/CarouselEffect'
+
+function Landing() {
+  return (
+    <LayOut>
+        <CarouselEffect/>
+        <Category/>
+        <Product/>
+    </LayOut>
+  )
+}
+
+export default Landing
