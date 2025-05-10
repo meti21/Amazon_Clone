dc777ff65d990aab600c581a13529fc2e61a0def
meti21 <metiwol@gmail.com>
2025-05-10 00:11:25 -0600

    add the category

--- src/Components/Category/catagoryData.js
+++ src/Components/Category/catagoryData.js
@@ -1,1 +1,22 @@
-
+export const categoryInfos = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imgLink: "https://m.media-amazon.com/images/I/812hhClRNaL._AC_SX679_.jpg",
+  },
+  {
+    title: "Men's Clothing",
+    name: "men's clothing",
+    imgLink: "https://m.media-amazon.com/images/I/81d-i2QomUL._AC_SY879_.jpg",
+  },
+  {
+    title: "Women's Clothing",
+    name: "women's clothing",
+    imgLink: "https://m.media-amazon.com/images/I/71e4RejKFuL._AC_SY879_.jpg",
+  },
+  {
+    title: "Jewelry",
+    name: "jewelery",
+    imgLink: "https://m.media-amazon.com/images/I/71l05yvK8XL._AC_SX679_.jpg",
+  },
+];
