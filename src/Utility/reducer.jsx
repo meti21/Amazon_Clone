6df9b4f70dbff0785a7f6b029596dab9e978e7ed
meti21 <metiwol@gmail.com>
2025-05-12 15:56:16 -0600

    feat: add additional case to remove item from basket

--- src/Utility/reducer.jsx
+++ src/Utility/reducer.jsx
@@ -20,13 +20,30 @@ export const reducer = (state, action) => {
           basket: [...state.basket, { ...action.item, amount: 1 }],
         };
       } else {
-        const updatedBasket = state.basket.map((item) => (
-           item.id === action.item.id
+        const updatedBasket = state.basket.map((item) =>
+          item.id === action.item.id
             ? { ...item, amount: item.amount + 1 }
             : item
-        ));
+        );
         return { ...state, basket: updatedBasket };
       }
+
+    case Type.REMOVE_FROM_BASKET_KEY:
+      const index = state.basket.findIndex((item) => item.id === action.id);
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+          if (newBasket[index].amount > 1) {
+            newBasket[index] = {
+              ...newBasket[index],
+              amount: newBasket[index].amount - 1,
+            };
+          } else {
+            newBasket.splice(index, 1);
+          }
+      }
+
+      return { ...state, basket: newBasket };
 
     default:
       return state;
