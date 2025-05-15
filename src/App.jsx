f3d537ae208ed7e9fc32a639f4a3c469d0af2c91
meti21 <metiwol@gmail.com>
2025-05-15 16:07:41 -0600

    Add Firebase auth state listener to persist user login across refresh

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,24 @@ import Header from './Components/Header/Header'
 
 import './App.css'
 import Routing from './Routing'
+import { useContext,useEffect } from 'react'
+import { DataContext } from './Components/DataProvider/DataProvider'
+import { Type } from './Utility/action.type'
+import { auth } from './Utility/firebase'
+
 
 function App() {
+  const [{ user }, dispatch] = useContext(DataContext);
+
+  // Listen for Firebase auth state changes and set the global user; without this, our app won't remember logged-in users after refresh or route change.
+  useEffect(() => {
+    return auth.onAuthStateChanged((authUser) => {
+      dispatch({
+        type: Type.SET_USER_KEY,
+        user: authUser || null,
+      });
+    });
+  }, []);
 
   return (
     <>
@@ -16,9 +32,9 @@ function App() {
       <Category/>
       <Product/> */}
 
-      <Routing/>
+      <Routing />
     </>
-  )
+  );
 }
 
 export default App
