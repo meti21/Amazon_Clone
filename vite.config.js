f6a35d136c9b125608174f61ab33e78325d8a32d
meti21 <metiwol@gmail.com>
2025-05-30 00:04:28 -0600

    fixed index.css path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "./", // crucial for relative asset paths on Vercel
   plugins: [react()],
 });
